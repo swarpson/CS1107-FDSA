72845f58ca087244b5c711b611a0e0d5468e7332
Swaroop R <swarooprbca24@rvu.edu.in>
2025-03-26 11:03:11 +0530

    Update P4a4.py

--- Lab3/P4a4.py
+++ Lab3/P4a4.py
@@ -3,77 +3,56 @@ class Node:
         self.data = data
         self.next = None
 
-class LinkedList:
+class SinglyLinkedList:
     def __init__(self):
         self.head = None
-
-    def insert_at_beginning(self, data):
-        new_node = Node(data)
-        new_node.next = self.head
-        self.head = new_node
-
-    def insert_at_end(self, data):
+    
+    # Method to append a node to the linked list
+    def append(self, data):
         new_node = Node(data)
         if self.head is None:
-            self.head = new_node 
-            return
-        
-        last = self.head
-        while last.next: 
-            last = last.next 
-        last.next = new_node 
-        
-    def delete_node(self, data): 
-        current = self.head
-        if current and current.data == data: 
-            self.head = current.next 
-            current = None 
-            return
-        
-        prev = None 
-        while current and current.data != data: 
-            prev = current 
-            current = current.next 
-        
-        if current is None: 
-            return 
+            self.head = new_node
+        else:
+            last_node = self.head
+            while last_node.next:
+                last_node = last_node.next
+            last_node.next = new_node
     
-        prev.next = current.next 
-        current = None 
-        
-    def traverse(self): 
-        current = self.head 
-        while current: 
-            print(current.data, end=" -> " if current.next else "")
-            current = current.next 
-        print()
+    # Method to search for a node by value
+    def search(self, data):
+        current_node = self.head
+        while current_node:
+            if current_node.data == data:
+                return True
+            current_node = current_node.next
+        return False
+    
+    # Method to display the linked list
+    def display(self):
+        if self.head is None:
+            print("The list is empty.")
+            return
+        current_node = self.head
+        while current_node:
+            print(current_node.data, end="->")
+            current_node = current_node.next
+        print("None")
 
-# Inputs
+# Using Inputs
+sll = SinglyLinkedList()
 
-linked_list = LinkedList()
+# Append nodes
+sll.append(30)
+sll.append(60)
+sll.append(90)
 
-# Insert elements at the beginning
-linked_list.insert_at_beginning(10)
-linked_list.insert_at_beginning(20)
-linked_list.insert_at_beginning(30)
-print("Linked List after inserting 30, 20, 10 at the beginning:")
-linked_list.traverse()
+# Search for a value in the list taking a value from the user
+search_value = int(input("Enter a value to check in the list:"))
+if sll.search(search_value):
+    print(f"Node with value {search_value} found in the list.")
+else:
+    print(f"Node with value {search_value} not found in the list.")
 
-# Insert elements at the end
-linked_list.insert_at_end(5)
-linked_list.insert_at_end(10)
-linked_list.insert_at_end(15)
-linked_list.insert_at_end(20)
-print("\nLinked List after inserting 5, 10, 15, 20 at the end:")
-linked_list.traverse()
-
-# Delete node
-linked_list.insert_at_end(30)
-linked_list.insert_at_end(40)
-linked_list.delete_node(20)
-print("\nLinked List after deleting node with data 20:")
-linked_list.traverse()
-
-# Traverse the list
-print("\nTraversing the linked list:")
-linked_list.traverse()
+# Display the list
+print("Linked List:")
+sll.display()
